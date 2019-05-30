582695bb80223afca106a5d4442978064b50dc89
Kan Dang <kand1@uci.edu>
2019-05-30 15:41:58 -0700

    Finish the Tutorial Series
    changed color scheme
    'added about me images
    added social media images and link

---
+++ js/parallax.js
@@ -0,0 +1,17 @@
+$(window).scroll(function(){
+  parallax();
+})
+
+
+function parallax() {
+
+  var wScroll = $(window).scrollTop();
+  // console.log(wScroll);
+
+  $('.parallax--bg').css('background-position',
+  'center '+(wScroll*0.75)+'px');
+
+  $('.parallax--box').css('top',
+  -5+(wScroll*0.0085)+'em')
+
+}
